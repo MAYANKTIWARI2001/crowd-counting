032b268a99a20d5bb1e0d44e719d1e9322770333
mayank tiwari <64486175+MAYANKTIWARI2001@users.noreply.github.com>
2023-09-12 13:13:28 +0530

    Create index.py
    Crowd counting is a crucial task in computer vision with applications in crowd management, security, and urban planning. This abstract introduces a novel approach to crowd counting using the YOLO (You Only Look Once) object detection framework and Python. YOLO's real-time object detection capabilities are leveraged to efficiently identify and count individuals within crowded scenes. The proposed method combines YOLO's object detection results with advanced counting algorithms, providing an accurate and efficient solution for crowd counting tasks. This abstract highlights the potential of this approach in addressing the challenges of crowd counting and its practical applications in various domains.

---
+++ index.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+cap= cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+camera = "https://192.168.235.149:8080/video"
+cap.open(camera)
+net =cv2.dnn.readNet("dnn_model/yolov4-tiny.weights","dnn_model/yolov4-tiny.cfg")
+model= cv2.dnn.DetectionModel(net)
+model.setInputParams(size=(320,320),scale=1/255)
+
+classes=[]
+with open("dnn_model/classes.txt","r") as file_object:
+  for class_name in file_object.readlines():
+    class_name = class_name.strip()
+    classes.append(class_name)
+  while True:
+    _,frame = cap.read()
+    height , width , _ = frame.shape
+    frame[913:height -5, 89:89+1093]=(255,255,255)
+    (class_ids,score,bboxes)=model.detect(frame,nmsThreshold=0.4)
+    i=0
+    for class_id,score,bbox in zip(class_ids,score,bboxes):
+      (x,y,w,h)= bbox
+      class_name=classes[class_id]
+      if class_name=="person":
+        i=i+1
+        cv2.putText(frame, str(class_name) + str(i), (x-10, y-10),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0, 0, 255), 2)
+
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(200,0,50),3)
+        
+        print(class_ids)
+        
+        polygon = np.array([[(20,20),(220,20),(220,70),(20,70)]])
+        
+        cv2.fillPoly(frame,polygon,(0,0,200))
+        
+        cv2.putText(frame, "PERSON : " + str(i), (30,60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+        cv2.imshow("camera", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+        cap.release()
+        cv2.destroyAllWindows()
+
